57b0f5215866a7680a1372f228667ee643de4ac4
pshoki <shokipsh@gmail.com>
2023-04-07 11:26:23 +0200

    add: barbecue plugin and tweak theme colors

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -8,6 +8,12 @@ return {
         keywordStyle = { italic = false },
         statementStyle = { bold = true, italic = false },
         commentStyle = { italic = false },
+        colors = {
+          palette = {
+            dragonBlack4 = "#1F1C1C",
+            dragonBlack6 = "#272523",
+          },
+        },
       })
 
       vim.cmd("colorscheme kanagawa-dragon")
