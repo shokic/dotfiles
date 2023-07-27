51891b469dac4f98aab5ca540547c4488ffaa643
pshoki <shokipsh@gmail.com>
2023-07-28 00:45:06 +0200

    change: theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,34 +1,18 @@
 return {
-    "catppuccin/nvim",
-    name = "catppuccin",
+    "rebelot/kanagawa.nvim",
     priority = 1000,
     config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            no_italic = true,
-            no_bold = true,
-            color_overrides = {
-                all = {
-                    base = "#181818",
-                },
-            },
-            integrations = {
-                mason = true,
-                neotree = true,
-                neogit = true,
-                cmp = true,
-                lsp_trouble = true,
-                which_key = true,
-                telescope = {
-                    enabled = true,
-                    style = "nvchad",
-                },
-                indent_blankline = {
-                    enabled = true,
-                    colored_indent_levels = false,
+        require("kanagawa").setup({
+            statementStyle = { bold = false },
+            keywordStyle = { italic = false },
+            commentStyle = { italic = false },
+            colors = {
+                palette = {
+                    dragonBlack3 = "#181818",
+                    dragonBlack4 = "#181818",
                 },
             },
         })
-        vim.cmd.colorscheme("catppuccin")
+        vim.cmd.colorscheme("kanagawa-dragon")
     end,
 }
