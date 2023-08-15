7d4fba421beb864a1deacac218f81e6c76fc3e96
pshoki <shokipsh@gmail.com>
2023-08-15 22:28:33 +0200

    change: theme

--- nvim/lua/plugins/theme.lua
+++ nvim/lua/plugins/theme.lua
@@ -1,17 +1,7 @@
 return {
-    "catppuccin/nvim",
+    "Mofiqul/vscode.nvim",
     priority = 1000,
     config = function()
-        require("catppuccin").setup({
-            flavour = "mocha",
-            no_italic = true,
-            no_bold = true,
-            color_overrides = {
-                all = {
-                    base = "#1f1f1f",
-                },
-            },
-        })
-        vim.cmd.colorscheme("catppuccin")
+        require("vscode").load()
     end,
 }
