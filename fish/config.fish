09b391bceb298ec2974cce3312fe5e499f71efbe
pshoki <shokipsh@gmail.com>
2023-11-13 00:51:55 +0100

    feat: find and open command in fish

--- fish/config.fish
+++ fish/config.fish
@@ -8,9 +8,17 @@ set --global hydro_symbol_prompt "> "
 
 alias ls="exa --icons"
 alias cat="bat"
+alias c="code"
 
 alias dev-sandbox="aws-vault exec dev-sandbox --"
 
 set -gx PATH "/opt/homebrew/bin" $PATH
 set -gx VOLTA_HOME "$HOME/.volta"
 set -gx PATH "$VOLTA_HOME/bin" $PATH
+set -gx FZF_DEFAULT_COMMAND "fd --type f --strip-cwd-prefix"
+
+function f  
+    set -l selected_dir (fd -t d | fzf)
+    set -l tab_title (string replace -r '.*\/(.+\/.+\/)' '$1' $selected_dir)
+    kitty @ launch --tab-title "$tab_title" --type tab --cwd current --hold fish -c "cd $selected_dir; exec fish" > /dev/null 2>&1
+end
