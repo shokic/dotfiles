9bc51148659e31687067a14bf52846098b22fb6e
kyshx <shokipsh@gmail.com>
2024-12-27 16:35:45 +0100

    remove: tmux

--- link.sh
+++ link.sh
@@ -1,6 +1,4 @@
 #!/bin/bash
 
-ln -snf ~/repos/dotfiles/kitty/* ~/.config/kitty
 ln -snf ~/repos/dotfiles/fish/* ~/.config/fish
-ln -snf ~/repos/dotfiles/tmux/* ~/.config/tmux
 ln -snf ~/repos/dotfiles/ghostty/* ~/.config/ghostty
