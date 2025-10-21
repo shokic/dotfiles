657211c93e6bc62fbad0c6cbad5ab869f51b76dd
shokic <shokipsh@gmail.com>
2025-10-21 10:52:05 +0200

    fixes

--- fish/config.fish
+++ fish/config.fish
@@ -10,21 +10,18 @@ alias ls="eza --icons"
 alias cat="bat"
 alias vim="nvim"
 
-function se
-    sesh connect $(sesh list | fzf)
-end
-
-set -gx VOLTA_HOME "$HOME/.volta"
-set -gx PATH "$VOLTA_HOME/bin" $PATH
-
 # prompt
 set tide_git_truncation_length 50
 
 zoxide init fish --cmd cd | source
 
-# bun
-set --export BUN_INSTALL "$HOME/.bun"
-set --export PATH $BUN_INSTALL/bin $PATH
-
 # For Brewfile
 set -gx HOMEBREW_BUNDLE_FILE "$HOME/.config/Brewfile"
+
+# For Volta
+set -gx VOLTA_HOME "$HOME/.volta"
+set -gx PATH "$VOLTA_HOME/bin" $PATH
+
+# For AWS
+alias koala-dev="aws-vault exec koala-dev --"
+alias koala-devops="aws-vault exec koala-devops --"
